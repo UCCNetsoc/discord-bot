bab63add0e2d969d9814d942497a9ddff2c7c5e8
Eric Moynihan <git@moynihan.io>
2020-04-07 18:18:45 +0100

    Added connection

--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
-module github.com/UCCNetsoc/discord-microservices
+module github.com/UCCNetsoc/discord-bot
 
 go 1.14
 
 require (
 	github.com/bwmarrin/discordgo v0.20.2
-	github.com/hashicorp/vault/api v1.0.4 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -20,8 +22,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	session, err := discordgo.New("")
+	session, err := discordgo.New("Bot " + token)
 	if err != nil {
 		panic("Couldn't init discord connection")
 	}
+	// Open websocket
+	err = session.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// Maintain connection until a SIGTERM, then cleanly exit
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	session.Close()
 }
