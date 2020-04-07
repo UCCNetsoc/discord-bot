f0022ca2b6c274d56bccfdbadb1f576a96fad980
Eric Moynihan <git@moynihan.io>
2020-04-07 17:22:03 +0100

    Stat design of le bot

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-var session *discordgo.Session
+func getEnv(key string) (string, error) {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return "", fmt.Errorf("Error finding %s", key)
+	}
+	return val, nil
+}
 
 func main() {
-	// TODO: Get credentials from vault
-	var err error
-	session, err = discordgo.New("")
+	token, err := getEnv("DISCORD_TOKEN")
+	if err != nil {
+		panic(err)
+	}
+	session, err := discordgo.New("")
 	if err != nil {
 		panic("Couldn't init discord connection")
 	}
