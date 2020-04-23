8b7a24e14864a9b37179f4e786a5f099478ac6ef
Eric Moynihan <git@moynihan.io>
2020-04-23 01:35:53 +0100

    Add print all vars

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -12,7 +13,26 @@ func InitConfig() error {
 	initDefaults()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // For gamers only
 	viper.AutomaticEnv()
-
 	// Consul
+	printAll()
 	return readFromConsul()
+}
+
+func printAll() {
+	fmt.Println("Startup variables:")
+	for k, v := range viper.AllSettings() {
+		fmt.Println(k + ":")
+		for sk, sv := range v.(map[string]interface{}) {
+			if strval, ok := sv.(string); ok {
+				if len(strval) > 3 {
+					fmt.Printf("%s: %s\n", sk, strval[:3])
+				} else {
+					fmt.Printf("%s: %s\n", sk, strval)
+				}
+			} else {
+
+				fmt.Printf("%s: %v\n", sk, sv)
+			}
+		}
+	}
 }
