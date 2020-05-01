43352575cd0a88055c4c68324de1c2afb8b45e3d
Eric Moynihan <git@moynihan.io>
2020-05-01 21:40:44 +0100

    Added api for Announcements

--- api/api.go
+++ api/api.go
@@ -20,6 +20,11 @@ type returnEvent struct {
 	ImageURL string `json:"image_url"`
 	Date     int64
 }
+type returnAnnouncement struct {
+	Date     int64
+	Content  string
+	ImageURL string `json:"image_url"`
+}
 
 var (
 	cached  *cache.Cache
@@ -96,5 +101,62 @@ func getEvents(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAnnouncements(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	limit := viper.GetInt("api.announcement_query_limit")
+	queryAmount, exists := query["q"]
+	if !exists || len(query) == 0 {
+		http.Error(w, "Please add the parameter 'q'", 403)
+		return
+	}
+	amount, err := strconv.Atoi(queryAmount[0])
+	if err != nil || amount > limit {
+		http.Error(w, "Please provide an int as 'q's value", 403)
+		return
+	}
+	var announcemenents []*Announcement
+	cachedAnnouncements, found := cached.Get("announcements")
+	if found {
+		announcemenents = cachedAnnouncements.([]*Announcement)
+	} else {
+		announcemenents = []*Announcement{}
+		channelID := viper.Get("discord.channels").(*config.Channels).PrivateEvents
+		liveAnnounce, err := session.ChannelMessages(channelID, 100, "", "", "")
+		if err != nil {
+			log.WithError(err).Error("Error querying events for api")
+			return
+		}
+		i := 0
+		for _, event := range liveAnnounce {
+			if i == amount {
+				break
+			}
+			parsed, err := ParseAnnouncement(&discordgo.MessageCreate{Message: event}, "")
+			if err == nil {
+				// Message successfully parsed as an event.
+				announcemenents = append(announcemenents, parsed)
+				i++
+			}
+		}
+		cached.Set("announcements", announcemenents, cache.DefaultExpiration)
+	}
+	w.Header().Set("content-type", "application/json")
+	returnAnnouncements := []returnAnnouncement{}
+	for _, ann := range announcemenents {
+		announce := returnAnnouncement{
+			Date:    ann.Date.Unix(),
+			Content: ann.Content,
+		}
+		if ann.Image != nil {
+			announce.ImageURL = ann.Image.Request.URL.String()
+		}
+		returnAnnouncements = append(returnAnnouncements, announce)
+	}
+
+	b, err := json.Marshal(returnAnnouncements)
+	if err != nil {
+		log.WithFields(log.Fields{"announcements": returnAnnouncements}).WithError(err).Error("Error marshalling announcements")
+		return
+	}
+	w.Write(b)
 
 }
