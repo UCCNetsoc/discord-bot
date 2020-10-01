bba6ec24d8f6101f47a2e3da6b0e9d24b08cf670
Eric Moynihan <git@moynihan.io>
2020-10-01 01:41:17 +0100

    Added query to announcements endpoint that gets messages from Public Announcements

--- api/shared.go
+++ api/shared.go
@@ -2,7 +2,11 @@ package api
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 // Entry represents an entry in the announcements channel
@@ -36,4 +40,33 @@ func (a Announcement) GetImage() *Image {
 // GetImage returns Image response data
 func (e Event) GetImage() *Image {
 	return e.Image
+}
+
+func parseImage(m *discordgo.Message) (*Image, error) {
+	var (
+		image       *http.Response
+		err         error
+		imageHeader *http.Header
+		imageURL    string
+		imageBody   *bytes.Buffer
+	)
+	if len(m.Attachments) > 0 && m.Attachments[0].Width > 0 {
+		image, err = http.Get(m.Attachments[0].URL)
+		if err != nil {
+			return nil, fmt.Errorf("Error parsing image: %w", err)
+		}
+		imageHeader = &image.Header
+		imageURL = image.Request.URL.String()
+		imageRead, err := ioutil.ReadAll(image.Body)
+		if err != nil {
+			return nil, err
+		}
+		imageBody = bytes.NewBuffer(imageRead)
+		defer image.Body.Close()
+	}
+	return &Image{
+		ImgData:   imageBody,
+		ImgURL:    imageURL,
+		ImgHeader: imageHeader,
+	}, nil
 }
