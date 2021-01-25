c3101db4e963f21befd398e88d461e7d82fe9891
Eric Moynihan <git@moynihan.io>
2021-01-25 17:02:56 +0000

    Added message for new vaccines

--- corona/corona.go
+++ corona/corona.go
@@ -332,6 +332,7 @@ func Listen(s *discordgo.Session) error {
 			currentDateVaccines = &vaccines.Date
 		} else if vaccines.Date.Unix() > currentDateVaccines.Unix() {
 			// New vaccines found.
+			s.ChannelMessageSend(channelID, "The HSE has released new vaccine numbers for Ireland:")
 			s.ChannelMessageSendEmbed(channelID, vaccines.Embed())
 		}
 		<-time.After(sleepTime)
