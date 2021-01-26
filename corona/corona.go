ba5373bb0878e4829def2312aa14265fa766f8bf
Eric Moynihan <git@moynihan.io>
2021-01-26 10:59:54 +0000

    Fix new vaccines found bug

--- corona/corona.go
+++ corona/corona.go
@@ -332,8 +332,9 @@ func Listen(s *discordgo.Session) error {
 			currentDateVaccines = &vaccines.Date
 		} else if vaccines.Date.Unix() > currentDateVaccines.Unix() {
 			// New vaccines found.
-			// s.ChannelMessageSend(channelID, "The HSE has released new vaccine numbers for Ireland:")
-			// s.ChannelMessageSendEmbed(channelID, vaccines.Embed())
+			currentDateVaccines = &vaccines.Date
+			s.ChannelMessageSend(channelID, "The HSE has released new vaccine numbers for Ireland:")
+			s.ChannelMessageSendEmbed(channelID, vaccines.Embed())
 		}
 		<-time.After(sleepTime)
 	}
