3c4e7c796e020cde02b8ecb6408b504539e3bf4e
Eric Moynihan <git@moynihan.io>
2021-01-31 16:22:36 +0000

    Add nitro event

--- commands/registerCommands.go
+++ commands/registerCommands.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/Strum355/log"
 	"github.com/UCCNetsoc/discord-bot/api"
+	"github.com/UCCNetsoc/discord-bot/config"
+	"github.com/UCCNetsoc/discord-bot/embed"
 	"github.com/UCCNetsoc/discord-bot/prometheus"
 	"github.com/bwmarrin/discordgo"
 	"github.com/dghubble/oauth1"
@@ -120,6 +122,15 @@ func Register(s *discordgo.Session) {
 
 // Called whenever a message is sent in a server the bot has access to
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Type == discordgo.MessageTypeUserPremiumGuildSubscription && m.Author != nil {
+		em := embed.NewEmbed()
+		em.SetTitle("New Nitro Boost 🎉")
+		em.SetDescription(fmt.Sprintf("Thank you %s for boosting the server!", m.Author.Mention()))
+		em.SetColor(0xdccb01)
+		channels := viper.Get("discord.channels").(*config.Channels)
+		s.ChannelMessageSendEmbed(channels.PublicGeneral, em.MessageEmbed)
+		return
+	}
 	if m.Author.Bot {
 		return
 	}
