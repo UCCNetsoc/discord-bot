1874307607e41ad8ce3cad5fb73e1a9a5877d300
Eric Moynihan <git@moynihan.io>
2021-04-02 00:03:58 +0000

    Simplify corona update

--- corona/vaccines.go
+++ corona/vaccines.go
@@ -69,41 +69,22 @@ func (v *Vaccines) Embed(prev *Vaccines) *discordgo.MessageEmbed {
 		)
 	} else {
 		description = p.Sprintf(`
-				__**New**__
 				**First installment**: %d (+%d) (%.2f%% of population)
 				**Second installment**: %d (+%d) (%.2f%% of population)
 				**Fully vaccinated**: %d (+%d) (%.2f%% of population)
-				**Total Administered**: %d (+%d) (%.2f%% of population)
+				**Total Administered**: %d (+%d) 
 
-				***Pfizer***: %d (+%d) (%.2f%% of population)
-				***AstraZeneca***: %d (+%d) (%.2f%% of population)
-				***Moderna***: %d (+%d) (%.2f%% of population)
-
-				__**Previously**__
-				**First installment**: %d
-				**Second installment**: %d
-				**Fully vaccinated**: %d
-				**Total Administered**: %d 
-
-				***Pfizer***: %d
-				***AstraZeneca***: %d
-				***Moderna***: %d 
-			`,
+				***Pfizer***: %d (+%d) 
+                ***AstraZeneca***: %d (+%d) 
+                ***Moderna***: %d (+%d) 
+                `,
 			v.First, int64(math.Abs(float64(v.First-prev.First))), firstPercentage,
 			v.Second, int64(math.Abs(float64(v.Second-prev.Second))), secondPercentage,
 			v.Total, int64(math.Abs(float64(v.Total-prev.Total))), totalPercentage,
-			v.TotalAdmin, int64(math.Abs(float64(v.TotalAdmin-prev.TotalAdmin))), adminPercentage,
-			v.Pfizer, int64(math.Abs(float64(v.Pfizer-prev.Pfizer))), pfPercentage,
-			v.AstraZeneca, int64(math.Abs(float64(v.AstraZeneca-prev.AstraZeneca))), azPercentage,
-			v.Moderna, int64(math.Abs(float64(v.Moderna-prev.Moderna))), mdPercentage,
-
-			prev.First,
-			prev.Second,
-			prev.Total,
-			prev.TotalAdmin,
-			prev.Pfizer,
-			prev.AstraZeneca,
-			prev.Moderna,
+			v.TotalAdmin, int64(math.Abs(float64(v.TotalAdmin-prev.TotalAdmin))),
+			v.Pfizer, int64(math.Abs(float64(v.Pfizer-prev.Pfizer))),
+            v.AstraZeneca, int64(math.Abs(float64(v.AstraZeneca-prev.AstraZeneca))),
+			v.Moderna, int64(math.Abs(float64(v.Moderna-prev.Moderna))),
 		)
 	}
 	return embed.NewEmbed().SetTitle("Vaccines Rollout in Ireland").SetDescription(description).SetFooter(fmt.Sprintf("As of %s", v.Date.Format(layoutIE))).MessageEmbed
